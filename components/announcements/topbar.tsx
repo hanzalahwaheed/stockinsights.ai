2e76659312242466c5e6c1f5665b92d7880777b7
hanzalahwaheed <hanzalah.w@gmail.com>
2024-07-20 16:36:46 +0530

    topbar done

--- components/announcements/topbar.tsx
+++ components/announcements/topbar.tsx
@@ -1,8 +1,33 @@
+import Image from "next/image";
 import React from "react";
 
 const AnnouncementsTopbar = () => {
   return (
-    <div className="w-full h-[70px] bg-neutral-100 dark:bg-neutral-950">announcements-topbar</div>
+    <div className="w-full h-[70px] bg-neutral-100 dark:bg-neutral-950 flex items-center justify-between border-b border-[1.5px] border-neutral-200 dark:border-neutral-700">
+      <div className="flex">
+        <h1 className="text-[16px] font-semibold text-neutral-700 ml-[60px]">
+          Announcements Dashboard
+        </h1>
+        <div className="flex gap-[10px] ml-[14px]">
+          <Image
+            src={"/announcements-topbar/ai-powered.svg"}
+            width={15}
+            height={17}
+            alt="AI-Powered"
+          />
+          <h2 className="">AI-Powered</h2>
+        </div>
+      </div>
+      <div className="flex gap-[10px] mr-[60px]">
+        <Image
+          src={"/announcements-topbar/updates.svg"}
+          width={14}
+          height={10}
+          alt="AI-Powered"
+        />
+        <p className="text-neutral-700 text-[12px]">Updates every 15 mins</p>
+      </div>
+    </div>
   );
 };
 
--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -18,28 +18,28 @@ export default function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button variant="outline" size="icon" className="dark:border-neutral-600 border-[1px]">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 dark:text-neutral-300" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent align="end" className="dark:border-none">
         <DropdownMenuItem
           onClick={() => setTheme("light")}
-          className="bg-white dark:bg-neutral-900"
+          className="bg-white dark:bg-neutral-900 cursor-pointer"
         >
           Light
         </DropdownMenuItem>
         <DropdownMenuItem
           onClick={() => setTheme("dark")}
-          className="bg-white dark:bg-neutral-900"
+          className="bg-white dark:bg-neutral-900 cursor-pointer"
         >
           Dark
         </DropdownMenuItem>
         <DropdownMenuItem
           onClick={() => setTheme("system")}
-          className="bg-white dark:bg-neutral-900"
+          className="bg-white dark:bg-neutral-900 cursor-pointer"
         >
           System
         </DropdownMenuItem>
