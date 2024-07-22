27ffedf397b94b6a5080960e1abed826b3f51071
hanzalahwaheed <hanzalah.w@gmail.com>
2024-07-22 17:19:04 +0530

    modularise table header

---
+++ components/announcements/table-header-text.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TableHeaderText = ({ content }: { content: string }) => {
+  return <h3 className="font-medium text-[14px]">{content}</h3>;
+};
+
+export default TableHeaderText;
--- components/announcements/table-header.tsx
+++ components/announcements/table-header.tsx
@@ -1,27 +1,28 @@
 import React from "react";
+import TableHeaderText from "./table-header-text";
 
 const TableHeader = () => {
   return (
     <div className="w-full min-h-[90px] bg-white dark:bg-neutral-900 flex items-center border-b-[1px] border-neutral-200 dark:border-neutral-700 hover:bg-slate-100">
       <div className="w-[190px]  py-[10px] pl-[60px] gap-[10px] flex items-center">
-        <h3 className="font-medium text-[14px]">Company</h3>
+        <TableHeaderText content="Company" />
       </div>
 
       <div className="flex items-center w-[250px]  py-[10px] pl-[40px] gap-[10px] ">
-        <h3 className="font-medium text-[14px]">Announcement Type</h3>
+        <TableHeaderText content="Announcement Type" />
       </div>
 
       <div className="flex-1 p-[10px] px-[20px] gap-[10px]">
-        <h3 className="font-medium text-[14px]">Announcement Summary</h3>
+        <TableHeaderText content="Announcement Summary" />
       </div>
 
       <div className="flex items-center justify-center w-[69px]  py-[10px] gap-[10px]">
-        <h3 className="font-medium text-[14px]">Sentiment</h3>
+        <TableHeaderText content="Sentiment" />
       </div>
 
       <div className="flex items-center justify-center w-[151px] h-[80px] py-[10px] pl-[20px] gap-[10px] mr-[60px]">
         <div className="flex flex-col gap-2.5">
-          <h3 className="font-medium text-[14px]">Source</h3>
+          <TableHeaderText content="Source" />
         </div>
       </div>
     </div>
