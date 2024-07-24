5e2460d699b573a4dd3dabfac174fcdd437678a9
hanzalahwaheed <hanzalah.w@gmail.com>
2024-07-24 17:41:10 +0530

    header tag

--- components/header.tsx
+++ components/header.tsx
@@ -3,11 +3,11 @@ import ModeToggle from "./mode-toggle";
 
 const Header = () => {
   return (
-    <div className="w-screen flex justify-end h-[88px] border-b border-neutral-200 dark:border-neutral-700 dark:bg-neutral-950">
+    <header className="w-screen flex justify-end h-[88px] border-b border-neutral-200 dark:border-neutral-700 dark:bg-neutral-950">
       <div className="flex justify-center items-center mx-[34px]">
         <ModeToggle />
       </div>
-    </div>
+    </header>
   );
 };
 
